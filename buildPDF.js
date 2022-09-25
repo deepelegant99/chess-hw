2aa8ea9159f0192d95942feadc3f9eb5a9739918
Henry Crossley <crossh2001@yahoo.com>
2022-09-24 23:44:42 -0700

    pdf works

--- buildPDF.js
+++ buildPDF.js
@@ -3,3 +3,4 @@ const { jsPDF } = require("jspdf"); // will automatically load the node version
 const doc = new jsPDF('landscape');
 doc.text("Hello world!", 10, 10);
 doc.save("a4.pdf"); // will save the file in the current working directory
+console.log("hello world");
